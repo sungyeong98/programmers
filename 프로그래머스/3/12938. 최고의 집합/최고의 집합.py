dd3ab81bc8037f01eae97070b72be9f96d9aec5b
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-06-17 16:47:16 +0900

    [level 3] Title: 최고의 집합, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.py
@@ -0,0 +1,11 @@
+from itertools import combinations_with_replacement
+def solution(n, s):
+    answer = []
+    if s//n<=0:
+        return [-1]
+    answer=[s//n for _ in range(n)]
+    if s//n==0:
+        return sorted(answer)
+    for i in range(s%n):
+        answer[i]+=1
+    return sorted(answer)
