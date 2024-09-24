1f8fc4fbdffa1ef6626a4d211bf9dc0493166323
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-09-24 15:50:19 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.js
@@ -0,0 +1,11 @@
+function solution(a, b) {
+    if(a%2===1 && b%2===1){
+        return a**2+b**2
+    }
+    else if(a%2===1 || b%2===1){
+        return 2*(a+b)
+    }
+    else{
+        return a>b?a-b:b-a
+    }
+}
