7a2850ecbf9a24e58e65298ae46653b9f7995e1e
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-09-25 14:46:54 +0900

    [level 0] Title: 배열 만들기 1, Time: 10.02 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+class Solution {
+    public int[] solution(int n, int k) {
+        return IntStream.rangeClosed(1,n)
+                        .filter(num->num%k==0)
+                        .toArray();
+    }
+}
