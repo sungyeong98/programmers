cdce75ad9d90b268ad5d5396f3ff30afbe190c3d
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-09-30 16:22:48 +0900

    [level 0] Title: n 번째 원소부터, Time: 15.41 ms, Memory: 88.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -0,0 +1,6 @@
+import java.util.stream.IntStream;
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        return IntStream.range(n-1, num_list.length).map(i->num_list[i]).toArray();
+    }
+}
