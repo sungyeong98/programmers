7d307025edac229c31d5c78f7e88d407f0086d6f
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2025-05-14 21:27:09 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -10,3 +10,10 @@ def solution(progresses, speeds):
             result[-1]+=1
     
     return result
+# temp = 남은 작업 시간 정보를 담고 있는 리스트
+# result = 결과 리스트
+# max_day = 이전 작업들 중 소요된 최대 시간
+
+# temp를 for문으로 순회
+# 만약 현재 소요시간이 max_day보다 크다면, max_day를 갱신 + 결과 리스트에 1 추가(배포가 새로 됐다는 의미)
+# 아니라면, 결과 리스트의 마지막 값에 +1 (앞에 있는 작업이 오래 걸려서 같이 배포됐다는 의미)
