5c6c80972cbe805072dcdca872293826695a7581
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2025-05-17 23:01:24 +0900

    [level 3] Title: 단어 변환, Time: 6.58 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,15 +1,20 @@
 from difflib import SequenceMatcher
 from collections import deque
 def solution(begin, target, words):
-    answer,q,v=0,deque(),[0]*len(words)
-    q.append([begin,0])
-    while q:
-        word,n=q.popleft()
-        if word==target:
-            answer=n
+    result = 0
+    queue, visited = deque([(begin, 0)]), set()
+    
+    while queue:
+        current_word, cnt = queue.popleft()
+        
+        if current_word == target:
+            result = cnt
             break
-        for i in range(len(words)):
-            if not v[i] and SequenceMatcher(None,word,words[i]).ratio()==(len(begin)-1)/len(begin):
-                q.append([words[i],n+1])
-                v[i]=1
-    return answer
+        
+        for next_word in words:
+            if next_word not in visited and \
+            SequenceMatcher(None, current_word, next_word).ratio() == (len(begin) - 1) / len(begin):
+                visited.add(next_word)
+                queue.append((next_word, cnt + 1))
+    
+    return result
