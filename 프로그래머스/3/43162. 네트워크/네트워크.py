701b45073717e57ee968752a173b54750a90a1e7
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2025-05-20 19:16:34 +0900

    [level 3] Title: 네트워크, Time: 0.97 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,27 +1,30 @@
 from collections import defaultdict, deque
 def solution(n, computers):
+    # 네트워크 연결 상태를 저장하는 딕셔너리
+    # ex) networks = {0: [1, 2]} -> 0번 컴퓨터는 1번, 2번 컴퓨터와 연결되어 있음
     networks = defaultdict(list)
     for idx, network_list in enumerate(computers):
         for i, val in enumerate(network_list):
             if idx != i and val == 1:
                 networks[idx].append(i)
     
-    visited = set()
-    result = 0
+    visited = set()     # 방문 여부 확인용
+    result = 0          # 결과 저장용
+    # 총 n번 for문 반복 (컴퓨터 갯수 = n)
     for computer in range(n):
-        if computer in visited:
+        if computer in visited:     # 이미 방문한 컴퓨터면, 넘어가기
             continue
-        queue = deque([])
-        queue.append(computer)
+        queue = deque([])       # bfs용 queue
+        queue.append(computer)  # 현재 컴퓨터를 queue에 삽입
         
         while queue:
-            cur_computer = queue.popleft()
+            cur_computer = queue.popleft()  # 현재 컴퓨터
             
-            if cur_computer not in visited:
-                visited.add(cur_computer)
-                for next_computer in networks[cur_computer]:
-                    queue.append(next_computer)
+            if cur_computer not in visited: # 현재 컴퓨터가 방문하지 않은 상태면
+                visited.add(cur_computer)   # visited에 추가
+                for next_computer in networks[cur_computer]:    # 딕셔너리에 있는 값들을 for문으로 순회
+                    queue.append(next_computer)     # queue에 삽입
                     
-        result += 1
+        result += 1     # 연결되어 있는 네트워크의 갯수에 +1
     
     return result
