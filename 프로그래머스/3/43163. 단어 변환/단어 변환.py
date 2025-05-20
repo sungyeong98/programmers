476ab397c9fa5a631b57056bab83170989582d1c
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2025-05-20 23:46:03 +0900

    [level 3] Title: 단어 변환, Time: 6.64 ms, Memory: 9.5 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -5,13 +5,16 @@ def solution(begin, target, words):
     queue, visited = deque([(begin, 0)]), set()
     
     while queue:
-        current_word, cnt = queue.popleft()
+        current_word, cnt = queue.popleft()     # 현재 단어, 변환 횟수
         
-        if current_word == target:
+        if current_word == target:  # 현재 단어와 타겟 단어가 같을 때
             result = cnt
             break
         
         for next_word in words:
+            # 다음 단어를 방문하지 않은 상태이고,
+            # 현재 단어와 다음 단어가 1글자만 차이나는 경우
+            # SequenceMatcher = 문자열을 비교하여 얼마나 유사한지 값으로 보여주는 함수
             if next_word not in visited and \
             SequenceMatcher(None, current_word, next_word).ratio() == (len(begin) - 1) / len(begin):
                 visited.add(next_word)
