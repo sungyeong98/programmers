4d41b4a4dc1d67174935c0275e5750fbd4f34103
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2025-05-24 03:01:39 +0900

    [level 3] Title: 가장 먼 노드, Time: 69.56 ms, Memory: 30.7 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,20 +1,21 @@
-import sys
-from collections import deque
-INF=sys.maxsize
+from collections import defaultdict, deque
 def solution(n, edge):
-    answer = 0
-    temp={i:[] for i in range(1,n+1)}
-    for i,j in edge:
-        temp[i].append(j)
-        temp[j].append(i)
-    q,count=deque([]),[INF]*(n+1)
-    q.append(1)
-    count[1]=0
-    while q:
-        node=q.popleft()
-        for i in temp[node]:
-            if count[i]==INF:
-                count[i]=count[node]+1
-                q.append(i)
-    answer=count[1:].count(max(count[1:]))
-    return answer
+    graph = defaultdict(set)
+    
+    for a, b in edge:
+        graph[a].add(b)
+        graph[b].add(a)
+    
+    distance = [float('inf') for _ in range(n+1)]
+    queue = deque([1])
+    distance[1] = 0
+    
+    while queue:
+        cur_node = queue.popleft()
+        
+        for next_node in graph[cur_node]:
+            if distance[next_node] == float('inf'):
+                distance[next_node] = distance[cur_node] + 1
+                queue.append(next_node)
+    
+    return distance[1:].count(max(distance[1:]))
