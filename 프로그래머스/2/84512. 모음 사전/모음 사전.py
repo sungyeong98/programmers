43a0d20d6b3b39c3d00f5612279f2724d7ebc89c
sungyeong <149341161+sungyeong98@users.noreply.github.com>
2025-05-26 20:33:39 +0900

    [level 2] Title: 모음 사전, Time: 1.81 ms, Memory: 9.41 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,8 +1,8 @@
 from itertools import product
 def solution(word):
     a = ['A', 'E', 'I', 'O', 'U']
-    temp = []
+    temp = []   # 모음 조합을 저장할 리스트
     for i in range(1, len(a)+1):
-        for p in product(a, repeat=i):
+        for p in product(a, repeat=i):  # 모음 조합을 생성 및 저장
             temp.append("".join(p))
     return sorted(temp).index(word) + 1
