915f8d6b365e3daabe33cf4dfcb89dd5cc2ad3ab
sungyeong <149341161+sungyeong98@users.noreply.github.com>
2025-05-28 20:59:38 +0900

    [level 3] Title: 순위, Time: 90.98 ms, Memory: 9.56 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.py
+++ 프로그래머스/3/49191. 순위/순위.py
@@ -1,21 +1,19 @@
 def solution(n, results):
-    answer = n
-    temp=[['N']*n for _ in range(n)]
-    for i in range(0,len(temp)):
-        for j in range(0,len(temp)):
-            if i==j:
-                temp[i][j]=0
-    for i in results:
-        a,b=i
-        temp[a-1][b-1]=1
-        temp[b-1][a-1]=-1
-    for i in range(0,len(temp)):
-        for j in range(0,len(temp)):
-            for k in range(0,len(temp)):
-                if temp[j][i]==1 and temp[i][k]==1:
-                    temp[j][k]=1
-                    temp[k][j]=-1
-    for i in temp:
-        if 'N' in i:
-            answer-=1
-    return answer
+    temp = [[float("inf")]*n for _ in range(n)]
+    
+    for i in range(n):
+        temp[i][i] = 0          # 자기 자신은 0으로 처리
+    
+    for a, b in results:
+        temp[a-1][b-1] = 1      # 승리
+        temp[b-1][a-1] = -1     # 패배
+    
+    # Floyd-Warshall Algorithm
+    for k in range(n):          # 새 정점  
+        for i in range(n):      # 시작 노드
+            for j in range(n):  # 끝 노드
+                if temp[i][k] == 1 and temp[k][j] == 1:
+                    temp[i][j] = 1
+                    temp[j][i] = -1
+    
+    return n - sum([1 if float("inf") in i else 0 for i in temp])
