8bd9fc255d698691705cccdde486cd0f9031b154
sungyeong <149341161+sungyeong98@users.noreply.github.com>
2025-05-29 17:29:08 +0900

    [level 4] Title: 저자 별 카테고리 별 매출액 집계하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
+++ 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
@@ -1,24 +1,20 @@
-select
-    a.AUTHOR_ID as AUTHOR_ID,
-    b.AUTHOR_NAME as AUTHOR_NAME,
-    a.CATEGORY as CATEGORY,
-    sum(temp.sale*a.PRICE) as TOTAL_SALES
-from
-    (
-        select
-            extract(month from SALES_DATE) as month,
-            BOOK_ID,
-            sum(SALES) as sale
-        from BOOK_SALES
-        where extract(month from SALES_DATE)=1
-        group by extract(month from SALES_DATE),BOOK_ID
-    ) as temp,
-    BOOK as a,
-    AUTHOR as b
-where
-    a.AUTHOR_ID=b.AUTHOR_ID and
-    a.BOOK_ID=temp.BOOK_ID
-group by
-    b.AUTHOR_NAME, a.CATEGORY
-order by 
-    AUTHOR_ID,CATEGORY desc
+with temp1 as (
+    select BOOK_ID, SALES
+    from BOOK_SALES
+    where year(SALES_DATE) = 2022 and month(SALES_DATE) = 1
+), temp2 as (
+    select 
+        b.CATEGORY, 
+        b.AUTHOR_ID, 
+        sum(b.PRICE * t.SALES) as TOTAL_PRICE
+    from BOOK b
+    join temp1 t on b.BOOK_ID = t.BOOK_ID
+    group by AUTHOR_ID, CATEGORY
+)
+
+select 
+    a.AUTHOR_ID, a.AUTHOR_NAME, t.CATEGORY,
+    t.TOTAL_PRICE
+from AUTHOR a
+join temp2 t on a.AUTHOR_ID = t.AUTHOR_ID
+order by AUTHOR_ID, CATEGORY desc
