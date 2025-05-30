015698db0b2a7b41e45f7c678a3d8e46265be061
sungyeong <149341161+sungyeong98@users.noreply.github.com>
2025-05-30 21:39:00 +0900

    [level 3] Title: 이중우선순위큐, Time: 271.28 ms, Memory: 15.5 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,18 +1,18 @@
 from heapq import heappush, heappop
 def solution(operations):
-    temp1, temp2 = [], []
+    temp1, temp2 = [], []   # temp1 = 최소힙, temp2 = 최대힙
     
     for op in operations:
-        word, num = op.split(" ")
+        word, num = op.split(" ")   # 명령어, 숫자
         
-        if word == "I":
-            heappush(temp1, int(num))
-            heappush(temp2, -int(num))
-        elif word == "D" and num == "1" and temp1:
-            target = heappop(temp2)
-            temp1.remove(-target)
-        elif word == "D" and num == "-1" and temp1:
-            target = heappop(temp1)
-            temp2.remove(-target)
+        if word == "I":     # 숫자 삽입
+            heappush(temp1, int(num))   # 최대힙에 숫자 삽입
+            heappush(temp2, -int(num))  # 최소힙에 숫자 삽입
+        elif word == "D" and num == "1" and temp1:  # 최댓값 삭제 명령어
+            target = heappop(temp2) # 최대힙에서 최대값 추출
+            temp1.remove(-target)   # 최소힙에서 -target값 추출
+        elif word == "D" and num == "-1" and temp1: # 최솟값 삭제 명령어
+            target = heappop(temp1) # 최소힙에서 최솟값 추출
+            temp2.remove(-target)   # 최대힙에서 -target 추출
             
     return [max(temp1), min(temp1)] if temp1 else [0, 0]
