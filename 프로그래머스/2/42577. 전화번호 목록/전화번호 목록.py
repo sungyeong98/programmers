51280b8deada33c48b3c71a9e23c2d96a8092da3
sungyeong <149341161+sungyeong98@users.noreply.github.com>
2025-06-02 21:27:31 +0900

    [level 2] Title: 전화번호 목록, Time: 106.32 ms, Memory: 29.9 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,7 +1,8 @@
 def solution(phone_book):
-    pb, n = sorted(phone_book), len(phone_book)
+    pb, n = sorted(phone_book), len(phone_book)     # 전화번호 목록을 사전순으로 정렬
     
     for i in range(1, n):
+        # 이전의 번호와 비교하여 만약 현재 번호가 이전 번호로 시작을 한다면 false 반환
         if pb[i-1] == pb[i][:len(pb[i-1])]:    return False
     
     return True
