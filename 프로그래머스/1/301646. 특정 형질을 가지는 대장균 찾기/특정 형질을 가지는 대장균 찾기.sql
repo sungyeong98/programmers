e3ee17b92bef5c3f0cd5d30e6d9aa2d25aca87c8
sungyeong <149341161+sungyeong98@users.noreply.github.com>
2025-06-04 00:40:15 +0900

    [level 1] Title: 특정 형질을 가지는 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
+++ 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
@@ -1,8 +1,5 @@
-with cnt as (
-    select *
-    from ECOLI_DATA 
-    where
-        GENOTYPE & 2 = 0 and
-        (GENOTYPE & 1 = 1 or GENOTYPE & 4 = 4)
-)
-select count(*) as COUNT from cnt
+select count(*) as COUNT
+from ECOLI_DATA
+where GENOTYPE & 2 = 0 and (GENOTYPE & 1 = 1 or GENOTYPE & 4 = 4)
+-- 2번 형질은 없어야 함 -> 2번 비트는 0이어야 함
+-- 1번 형질 혹은 3번 형질은 존재해야함 -> 1번 비트, 4번 비트는 1 or 4이어야 함
