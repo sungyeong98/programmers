e47313323f4229ff3b43a5e3de4a6e0a47d4801a
sungyeong <149341161+sungyeong98@users.noreply.github.com>
2025-06-04 23:58:52 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,25 +1,6 @@
-with convert_bin as (
-    select ID, EMAIL, FIRST_NAME, LAST_NAME, conv(SKILL_CODE,10,2) as num
-    from DEVELOPERS 
-), reverse_bin as (
-    select ID, EMAIL, FIRST_NAME, LAST_NAME, reverse(num) as rev_num
-    from convert_bin
-), pos_1 as (
-    select
-        log2(CODE)+1 as position
-    from SKILLCODES
-    where NAME='C#'
-), pos_2 as (
-    select
-        log2(CODE)+1 as position
-    from SKILLCODES
-    where NAME='Python'
-), result as (
-    select ID, EMAIL, FIRST_NAME, LAST_NAME
-    from reverse_bin
-    where 
-        substring(rev_num,(select position from pos_1),1)='1' or
-        substring(rev_num,(select position from pos_2),1)='1'
-)
-select * from result
+select ID, EMAIL, FIRST_NAME, LAST_NAME
+from DEVELOPERS
+where 
+    SKILL_CODE & (select CODE from SKILLCODES where NAME = "C#") != 0 or
+    SKILL_CODE & (select CODE from SKILLCODES where NAME = "Python") != 0
 order by ID
