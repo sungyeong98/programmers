59a04eb90efb94a2159f261f9c4deb0023d6d018
sungyeong <149341161+sungyeong98@users.noreply.github.com>
2025-06-05 00:10:36 +0900

    [level 3] Title: 단속카메라, Time: 0.86 ms, Memory: 9.67 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -1,10 +1,10 @@
 def solution(routes):
     result = 0
-    temp = float("-inf")
-    r = sorted(routes, key = lambda x:x[1])
+    temp = float("-inf")    # 카메라 설치 위치
+    r = sorted(routes, key = lambda x:x[1])     # 고속도로를 빠져나오는 시간을 기준으로 정렬
     
     for start, end in r:
-        if temp < start:
+        if temp < start:    # 카메라의 위치보다 현재 차량의 진입 위치가 크다면, 카메라를 해당 차량의 탈출 시점에 설치
             temp = end
             result += 1
     
