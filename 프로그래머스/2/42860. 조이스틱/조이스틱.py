50784e1cef1ded89d02768a129f0a062dd0eb838
sungyeong <149341161+sungyeong98@users.noreply.github.com>
2025-06-04 21:09:44 +0900

    [level 2] Title: 조이스틱, Time: 0.02 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -1,14 +1,24 @@
-from string import ascii_uppercase
-alp={val:idx if idx<len(list(ascii_uppercase))//2 else len(list(ascii_uppercase))-idx for idx,val in enumerate(list(ascii_uppercase))}
-
 def solution(name):
-    answer = 0
-    move=len(name)-1
-    for idx,val in enumerate(name):
-        answer+=alp[val]
-        temp=idx+1
-        while temp<len(name) and name[temp]=='A':
-            temp+=1
-        move=min([move,2*idx+len(name)-temp,idx+2*(len(name)-temp)])
-    answer+=move
-    return answer
+    temp = {chr(val):idx if idx<=13 else 26-idx for idx, val in enumerate(list(range(65, 91)))}
+    result = 0
+    n = len(name)
+    move = n - 1
+    
+    for idx, val in enumerate(name):
+        result += temp[val]
+        
+        next_idx = idx + 1     # 오른쪽 방향으로 A가 안나올때까지 이동
+        while next_idx < n and name[next_idx] == "A":
+            next_idx += 1
+            
+        # 오른쪽으로 idx까지 이동
+        # 0번째 인덱스에서 왼쪽으로 n - next_idx까지 이동 후 다시 후진
+        right_move = idx + 2 * (n - next_idx)
+        
+        # 0번째 인덱스에서 idx까지 이동 후 다시 후진
+        # 왼쪽으로 n - next_idx까지 이동
+        left_move = 2 * idx + (n - next_idx)
+        
+        move = min(move, left_move, right_move)
+    
+    return result + move
