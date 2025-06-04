344671069acd7ae8ee54d548c8bb617fc74668a3
sungyeong <149341161+sungyeong98@users.noreply.github.com>
2025-06-05 00:08:43 +0900

    [level 3] Title: 단속카메라, Time: 0.86 ms, Memory: 9.65 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -1,8 +1,11 @@
-def solution(r):
-    answer,temp=0,-30001
-    r.sort(key=lambda x:x[1])
-    for i in r:
-        if i[0]>temp:
-            answer+=1
-            temp=i[1]
-    return answer
+def solution(routes):
+    result = 0
+    temp = float("-inf")
+    r = sorted(routes, key = lambda x:x[1])
+    
+    for start, end in r:
+        if temp < start:
+            temp = end
+            result += 1
+    
+    return result
