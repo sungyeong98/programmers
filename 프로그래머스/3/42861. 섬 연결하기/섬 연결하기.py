d7ddc56b2804eeeed5a3441ae43f4821341b4910
sungyeong <149341161+sungyeong98@users.noreply.github.com>
2025-06-05 23:30:52 +0900

    [level 3] Title: 섬 연결하기, Time: 0.06 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,24 +1,24 @@
-def find_parent(parent, x):
-    if parent[x] != x:
-        parent[x] = find_parent(parent, parent[x])
-    return parent[x]
-
-def union_parent(parent, a, b):
-    a = find_parent(parent, a)
-    b = find_parent(parent, b)
-    if a<b:
-        parent[b] = a
-    else:
-        parent[a] = b
-
+from heapq import heappush, heappop
+from collections import defaultdict
 def solution(n, costs):
-    answer = 0
-    parent = [i for i in range(n)]
-    costs.sort(key = lambda x : x[2])
-
-    for node1, node2, cost in costs:
-        if find_parent(parent, node1) != find_parent(parent, node2):
-            union_parent(parent, node1, node2)
-            answer += cost 
-
-    return answer
+    graph = defaultdict(list)
+    visited = set()
+    result = 0
+    
+    for start, end, cost in costs:
+        graph[start].append((cost, end))
+        graph[end].append((cost, start))
+    
+    temp = [(0, 0)]
+    
+    while temp:
+        cost, cur_node = heappop(temp)
+        if cur_node in visited:
+            continue
+        visited.add(cur_node)
+        result += cost
+        
+        for next_cost, next_node in graph[cur_node]:
+            if next_node not in visited:
+                heappush(temp, (next_cost, next_node))
+    return result              
