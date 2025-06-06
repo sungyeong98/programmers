cd7d5257e2dc37efdc487e05037658548a57fe6d
sungyeong <149341161+sungyeong98@users.noreply.github.com>
2025-06-07 02:06:19 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.04 ms, Memory: 9.54 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.py
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.py
@@ -1,26 +1,33 @@
 def solution(video_len, pos, op_start, op_end, commands):
-    video_len, pos, op_start, op_end = convert(video_len), convert(pos), convert(op_start), convert(op_end)
+    total_time = minute_to_second(video_len)
+    start = minute_to_second(op_start)
+    end = minute_to_second(op_end)
+    current_time = query_skip(minute_to_second(pos), start, end)
     
-    if op_start<=pos<=op_end:
-        pos=op_end
+    for command in commands:
+        if command == "next":   current_time = query_next(current_time, total_time)
+        else:                   current_time = query_prev(current_time)
+        current_time = query_skip(current_time, start, end)
     
-    for i in commands:
-        if i=='next':
-            pos+=min(10, video_len-pos)
-        if i=='prev':
-            pos-=min(10, pos)
-        if op_start<=pos<=op_end:
-            pos=op_end
+    return second_to_minute(query_skip(current_time, start, end))
+
+def minute_to_second(time):
+    minute, second = time.split(":")
+    return int(minute) * 60 + int(second)
     
-    minute, second = str(pos//60), str(pos%60)
+def second_to_minute(time):
+    minute = str(time // 60)
+    second = str(time % 60)
     
-    if len(minute)==1:
-        minute = "0"+minute
-    if len(second)==1:
-        second = "0"+second
+    if len(minute) == 1:    minute = "0" + minute
+    if len(second) == 1:    second = "0" + second
     return minute + ":" + second
     
-def convert(time):
-    minute, second = time.split(':')
-    return int(minute)*60 + int(second)
-    
+def query_prev(time):
+    return time - 10 if time - 10 >= 0 else 0
+
+def query_next(time, end):
+    return time + 10 if end - time >= 10 else end
+
+def query_skip(time, start, end):
+    return end if start <= time <= end else time
