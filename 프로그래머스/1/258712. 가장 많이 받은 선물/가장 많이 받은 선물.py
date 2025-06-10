0005151873c7ce17cc9c39f7798ee0ad5938576b
sungyeong <149341161+sungyeong98@users.noreply.github.com>
2025-06-11 01:04:22 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 5.50 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
@@ -1,8 +1,8 @@
 from collections import defaultdict
 def solution(friends, gifts):
-    give_info = {key : {val : 0 for val in friends} for key in friends}
-    give_cnt = defaultdict(int)
-    receive_cnt = defaultdict(int)
+    give_info = {key : {val : 0 for val in friends} for key in friends} # 선물을 주고 받은 정보 저장
+    give_cnt = defaultdict(int)         # 선물을 준 횟수
+    receive_cnt = defaultdict(int)      # 선물을 받은 횟수
 
     for g in gifts:
         giver, receiver = g.split()
@@ -15,19 +15,19 @@ def solution(friends, gifts):
     for i in range(len(friends) - 1):
         for j in range(i + 1, len(friends)):
             p1, p2 = friends[i], friends[j]
-            p1_cnt = give_info[p1][p2]
-            p2_cnt = give_info[p2][p1]
-            p1_score = give_cnt[p1] - receive_cnt[p1]
-            p2_score = give_cnt[p2] - receive_cnt[p2]
+            p1_cnt = give_info[p1][p2]  # p1이 p2에게 준 선물 갯수
+            p2_cnt = give_info[p2][p1]  # p2가 p1에게 준 선물 갯수
+            p1_score = give_cnt[p1] - receive_cnt[p1]   # p1의 선물 지수
+            p2_score = give_cnt[p2] - receive_cnt[p2]   # p2의 선물 지수
 
-            if p1_cnt > p2_cnt:
+            if p1_cnt > p2_cnt:     # p1이 p2에게 더 많이 선물을 줬다면
                 result[p1] += 1
-            elif p1_cnt < p2_cnt:
+            elif p1_cnt < p2_cnt:   # p2가 p1에게 더 많이 선물을 줬다면
                 result[p2] += 1
-            else:
-                if p1_score > p2_score:
+            else:                   # 서로 선물을 주고 받지 않았다면
+                if p1_score > p2_score:     # p1의 선물 지수가 더 높다면
                     result[p1] += 1
-                elif p1_score < p2_score:
+                elif p1_score < p2_score:   # p2의 선물 지수가 더 높다면
                     result[p2] += 1
 
     return max(result.values()) if result else 0
