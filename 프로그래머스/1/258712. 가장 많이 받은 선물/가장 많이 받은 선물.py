ee8fecdd53e7d83e0d08a9fc7f431aa6f8795202
sungyeong <149341161+sungyeong98@users.noreply.github.com>
2025-06-11 00:58:12 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 7.80 ms, Memory: 9.8 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
@@ -1,45 +1,33 @@
-def solution(friends,gifts):
-    #서로 선물을 몇번 줬는지 횟수를 저장하기 위한 이중배열
-    gift_cnt=[[0 for _ in range(len(friends))] for _ in range(len(friends))]
+from collections import defaultdict
+def solution(friends, gifts):
+    give_info = {key : {val : 0 for val in friends} for key in friends}
+    give_cnt = defaultdict(int)
+    receive_cnt = defaultdict(int)
 
-    #이름 인덱스값을 불러오기 위한 해시
-    name_cnt={i: j for j,i in enumerate(friends)}
+    for g in gifts:
+        giver, receiver = g.split()
+        give_info[giver][receiver] += 1
+        give_cnt[giver] += 1
+        receive_cnt[receiver] += 1
 
-    #인덱스 값으로 이름을 불러오기 위한 해시
-    idx={i:j for i,j in enumerate(friends)}
+    result = defaultdict(int)
 
-    #선물 지수를 저장하기 위한 배열
-    gift_score=[0 for _ in range(len(friends))]
+    for i in range(len(friends) - 1):
+        for j in range(i + 1, len(friends)):
+            p1, p2 = friends[i], friends[j]
+            p1_cnt = give_info[p1][p2]
+            p2_cnt = give_info[p2][p1]
+            p1_score = give_cnt[p1] - receive_cnt[p1]
+            p2_score = give_cnt[p2] - receive_cnt[p2]
 
-    #총 받은 선물 갯수를 저장하기 위한 배열
-    total_cnt=[0 for _ in range(len(friends))]
+            if p1_cnt > p2_cnt:
+                result[p1] += 1
+            elif p1_cnt < p2_cnt:
+                result[p2] += 1
+            else:
+                if p1_score > p2_score:
+                    result[p1] += 1
+                elif p1_score < p2_score:
+                    result[p2] += 1
 
-    #gifts배열을 순회하면서 누가 누구에게 선물을 몇 번 줬는지 계산
-    for log in gifts:
-        i,j=log.split(' ')
-        gift_cnt[name_cnt[i]][name_cnt[j]]+=1
-
-    #준 선물 갯수와 받은 선물 갯수를 카운트, 선물 지수 계산 후 저장
-    for i in friends:
-        give_gift,take_gift=sum(gift_cnt[name_cnt[i]]),0
-        for j in range(len(friends)):
-            take_gift+=gift_cnt[j][name_cnt[i]]
-        score=give_gift-take_gift
-        gift_score[name_cnt[i]]=score
-    
-    #gift_cnt를 순회하면서 받을 선물을 카운트
-    for i in range(len(friends)):
-        for j in range(len(friends)):
-            if i!=j:
-                #서로 선물을 준 경우에 대해서 계산
-                if (gift_cnt[i][j]!=0 or gift_cnt[j][i]!=0) and gift_cnt[i][j]>gift_cnt[j][i]:
-                    total_cnt[i]+=1
-                #서로 같은 갯수의 선물을 주고받은 경우에 대해서 계산
-                elif (gift_cnt[i][j]!=0 or gift_cnt[j][i]!=0) and gift_cnt[i][j]==gift_cnt[j][i]:
-                    if gift_score[i]>gift_score[j]:
-                        total_cnt[i]+=1
-                #서로 선물을 주지 않은 경우에 대해서 계산
-                elif gift_cnt[i][j]==0 and gift_cnt[j][i]==0:
-                    if gift_score[i]>gift_score[j]:
-                        total_cnt[i]+=1
-    return max(total_cnt)
+    return max(result.values()) if result else 0
