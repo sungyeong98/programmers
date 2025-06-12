8905154c70233bd6c6b156feb600a24e6f28a451
sungyeong <149341161+sungyeong98@users.noreply.github.com>
2025-06-12 17:15:24 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -1,19 +1,10 @@
-with convert_bin as (
-    select ID, EMAIL, FIRST_NAME, LAST_NAME, conv(SKILL_CODE,10,2) as num
-    from DEVELOPERS
-), reverse_bin as (
-    select ID, EMAIL, FIRST_NAME, LAST_NAME, reverse(num) as rev_num
-    from convert_bin
-), find_pos as (
-    select log2(CODE) +1 as position
-    from SKILLCODES 
-    where CATEGORY='Front End'
-), temp as (
-    select ID, EMAIL, FIRST_NAME, LAST_NAME
-    from reverse_bin as a
-    join find_pos as b
-        on substring(a.rev_num, b.position, 1)='1'
+with temp as (
+    select sum(CODE) as CODE
+    from SKILLCODES
+    where CATEGORY = "Front End"
 )
-select * from temp
-group by ID, EMAIL, FIRST_NAME, LAST_NAME
+
+select d.ID, d.EMAIL, d.FIRST_NAME, d.LAST_NAME
+from DEVELOPERS d, temp t
+where d.SKILL_CODE & t.CODE > 0
 order by ID
