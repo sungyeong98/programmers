06873729345dab9d8153f38877f6684945e65a6c
sungyeong <149341161+sungyeong98@users.noreply.github.com>
2025-06-15 21:50:05 +0900

    [level 0] Title: 두 수의 연산값 비교하기, Time: 13.87 ms, Memory: 64.6 MB -BaekjoonHub

--- 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.kt
+++ 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.kt
@@ -1,6 +1,9 @@
-import kotlin.math.*
 class Solution {
     fun solution(a: Int, b: Int): Int {
-        return max((a.toString() + b.toString()).toInt(), 2*a*b)
+        val temp1: Int = (a.toString() + b.toString()).toInt()
+        val temp2: Int = 2 * a * b
+        val result = if (temp1 > temp2) temp1 else temp2
+        
+        return result
     }
 }
