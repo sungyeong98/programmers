702e4918d34a5588566133ffba1dae055cb0b9d9
sungyeong <149341161+sungyeong98@users.noreply.github.com>
2025-06-17 00:30:20 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 20.20 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/README.md
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/README.md
@@ -1,10 +1,10 @@
 # [level 3] [카카오 인턴] 경주로 건설 - 67259 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67259) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67259?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.20 ms
+메모리: 9.23 MB, 시간: 20.20 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 17일 16:45:05
+2025년 06월 17일 00:30:18
 
 ### 문제 설명
 
--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
@@ -1,24 +1,31 @@
 from collections import deque
-INF=int(1e9)
 def solution(board):
-    answer,n = INF,len(board)
-    dp=[[[INF for _ in range(n)] for _ in range(n)] for _ in range(4)]
-    direction=[[0,1,0],[1,0,1],[0,-1,2],[-1,0,3]]
-    q=deque([[0,0,0,0],[0,0,0,1]])
-    while q:
-        x,y,m,d=q.popleft()
-        for i in range(4):
-            nx=x+direction[i][0]
-            ny=y+direction[i][1]
-            if -1<nx<n and -1<ny<n and board[nx][ny]==0:
-                nm=m+1
-                if not d==direction[i][2]:
-                    nm+=5
-                if nm<dp[direction[i][2]][nx][ny]:
-                    dp[direction[i][2]][nx][ny]=nm
-                    if nx==n-1 and ny==n-1:
-                        continue
-                    q.append([nx,ny,nm,direction[i][2]])
-    for i in range(4):
-        answer=min([answer,dp[i][n-1][n-1]])
-    return answer*100
+    n = len(board)
+    result = float("inf")
+    
+    # 방향 - 아래(0), 오른쪽(1), 위(2), 왼쪽(3)
+    direction = [(1, 0, 0), (0, 1, 1), (-1, 0, 2), (0, -1, 3)]
+    distance = [[[float("inf") for _ in range(n)] for _ in range(n)] for _ in range(4)]
+    queue = deque([(0, 0, 0, 0), (0, 0, 0, 1)])
+
+    while queue:
+        x, y, cur_cost, cur_dir = queue.popleft()
+        
+        for dx, dy, next_dir in direction:
+            nx = x + dx
+            ny = y + dy
+            
+            if 0<= nx < n and 0<= ny < n and board[nx][ny] == 0:
+                next_cost = cur_cost + 100
+                
+                if cur_dir != next_dir:     next_cost += 500
+                
+                if distance[next_dir][nx][ny] > next_cost:
+                    distance[next_dir][nx][ny] = next_cost
+                    if (nx, ny) == (n-1, n-1):  continue
+                    
+                    queue.append((nx, ny, next_cost, next_dir))
+        
+    for i in range(4):      result = min(result, distance[i][n-1][n-1])
+    
+    return result
