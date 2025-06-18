7a084c08ec95b9688253a1e7fdf8a165384d9f47
sungyeong <149341161+sungyeong98@users.noreply.github.com>
2025-06-19 00:08:45 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -1,21 +1,13 @@
-with high as (
-    select
-        EMP_NO,
-        sum(SCORE) as sum_score
-    from HR_GRADE
+with temp1 as (
+    select EMP_NO, sum(SCORE) as SCORE
+    from HR_GRADE 
     group by EMP_NO
-    order by sum(SCORE) desc
+), temp2 as (
+    select t.SCORE, t.EMP_NO, e.EMP_NAME, e.POSITION, e.EMAIL
+    from temp1 t
+    join HR_EMPLOYEES e on t.EMP_NO = e.EMP_NO
+    order by SCORE desc
     limit 1
 )
 
-select
-    b.sum_score as SCORE,
-    a.EMP_NO as EMP_NO,
-    a.EMP_NAME as EMP_NAME,
-    a.POSITION as POSITION,
-    a.EMAIL as EMAIL
-from
-    HR_EMPLOYEES as a,
-    high as b
-where
-    a.EMP_NO=b.EMP_NO
+select * from temp2
