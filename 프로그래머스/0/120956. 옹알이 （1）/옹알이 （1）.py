121b77c53958d7ad45574b7a0a15ab50bb9c909d
sungyeong <149341161+sungyeong98@users.noreply.github.com>
2025-06-18 22:24:58 +0900

    [level 0] Title: 옹알이 (1), Time: 0.18 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -1,17 +1,12 @@
 from itertools import permutations
 def solution(babbling):
-    answer = 0
-    term=["aya", "ye", "woo", "ma"]
-    temp=[]
-    for i in range(1,5):
-        a=list(permutations(term,i))
-        for j in a:
-            b=''
-            for k in j:
-                b+=k
-            temp.append(b)
-    for i in temp:
-        for j in babbling:
-            if i==j:
-                answer+=1
-    return answer
+    word_list = ["aya", "ye", "woo", "ma"]
+    temp = []
+    for i in range(1, 5):
+        for p in permutations(word_list, i):
+            temp.append("".join(p))
+    result = 0
+    for i in babbling:
+        if i in temp:
+            result += 1
+    return result
