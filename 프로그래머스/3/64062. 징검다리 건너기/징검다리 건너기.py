26c56f4a7163290c5e995554822dfc4fe884b052
sungyeong <149341161+sungyeong98@users.noreply.github.com>
2025-06-19 00:44:19 +0900

    [level 3] Title: 징검다리 건너기, Time: 398.73 ms, Memory: 17.6 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -1,15 +1,20 @@
-from collections import deque
-import sys
-INF=sys.maxsize
 def solution(stones, k):
-    answer = INF
-    temp=deque()
-    for i in range(len(stones)):
-        while len(temp)>0 and temp[-1][0]<stones[i]:
-            temp.pop()
-        temp.append([stones[i],i])
-        while temp[0][1]<i-k+1:
-            temp.popleft()
-        if i+1>=k:
-            answer=min(answer,temp[0][0])
-    return answer
+    left, right = 1, max(stones)
+    result = 0
+    
+    while left <= right:
+        mid = (left + right) // 2
+        cnt = 0
+        
+        for i in stones:
+            if mid > i: 
+                cnt += 1
+                if cnt >= k: break
+            else:   cnt = 0
+        
+        if cnt < k:    
+            result = mid
+            left = mid + 1
+        else:
+            right = mid - 1
+    return result
