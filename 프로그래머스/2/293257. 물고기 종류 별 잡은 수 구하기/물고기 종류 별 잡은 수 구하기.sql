89ef5c51a0ca2b11e6f46e449a5a232ee0deb366
sungyeong <149341161+sungyeong98@users.noreply.github.com>
2025-06-19 23:18:47 +0900

    [level 2] Title: 물고기 종류 별 잡은 수 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/293257. 물고기 종류 별 잡은 수 구하기/물고기 종류 별 잡은 수 구하기.sql
+++ 프로그래머스/2/293257. 물고기 종류 별 잡은 수 구하기/물고기 종류 별 잡은 수 구하기.sql
@@ -1,7 +1,5 @@
-select
-    count(*) as FISH_COUNT,
-    b.FISH_NAME as FISH_NAME
-from FISH_INFO as a, FISH_NAME_INFO as b
-where a.FISH_TYPE=b.FISH_TYPE
-group by b.FISH_NAME
+select count(n.FISH_NAME) as FISH_COUNT, n.FISH_NAME
+from FISH_INFO i
+join FISH_NAME_INFO n on i.FISH_TYPE = n.FISH_TYPE
+group by n.FISH_NAME
 order by FISH_COUNT desc
