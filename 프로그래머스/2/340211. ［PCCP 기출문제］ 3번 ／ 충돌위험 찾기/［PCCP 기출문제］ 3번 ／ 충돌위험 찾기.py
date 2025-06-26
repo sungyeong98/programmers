5789241fd71a1627ece928c1b52c296a73702e36
sungyeong <149341161+sungyeong98@users.noreply.github.com>
2025-06-26 22:58:26 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 3030.55 ms, Memory: 352 MB -BaekjoonHub

---
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.py
@@ -0,0 +1,29 @@
+from collections import defaultdict
+def solution(points, routes):
+    load = defaultdict(lambda : defaultdict(set))
+    result = 0
+
+    for robot_id, route in enumerate(routes):
+        cnt = 0
+        for j in range(len(route) - 1):
+            (start_x, start_y) = points[route[j] - 1]
+            (end_x, end_y) = points[route[j + 1] - 1]
+
+            load[cnt][(start_x, start_y)].add(robot_id)
+
+            while start_x != end_x:
+                start_x += 1 if start_x < end_x else -1
+                cnt += 1
+                load[cnt][(start_x, start_y)].add(robot_id)
+
+            while start_y != end_y:
+                start_y += 1 if start_y < end_y else -1
+                cnt += 1
+                load[cnt][(start_x, start_y)].add(robot_id)
+
+    for time in load:
+        for loc in load[time]:
+            if len(load[time][loc]) >= 2:  # 로봇 2대 이상이면 위험
+                result += 1
+
+    return result
