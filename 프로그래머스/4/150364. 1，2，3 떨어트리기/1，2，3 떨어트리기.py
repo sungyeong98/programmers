67a522539e4d58cc386276dc4baf4c16a177a228
sungyeong <149341161+sungyeong98@users.noreply.github.com>
2025-06-28 22:44:04 +0900

    [level 4] Title: 1,2,3 떨어트리기, Time: 6.10 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/4/150364. 1，2，3 떨어트리기/1，2，3 떨어트리기.py
+++ 프로그래머스/4/150364. 1，2，3 떨어트리기/1，2，3 떨어트리기.py
@@ -1,55 +1,67 @@
 from collections import deque
 import math
-class tree:
-    def __init__(self,value):
-        self.parent=None
-        self.node=value
-        self.children=deque()
-def generate(edges,target):
-    temp=[None]*len(target)
-    for i,j in edges:
-        if temp[i-1] is None:
-            temp[i-1]=tree(i-1)
-        if temp[j-1] is None:
-            temp[j-1]=tree(j-1)
-    for i,j in edges:
-        temp[i-1].children.append(temp[j-1])
-        temp[i-1].children=deque(sorted(temp[i-1].children,key=lambda x:x.node))
-        temp[j-1].parent=temp[i-1]
+
+class Tree:
+    def __init__(self, value):
+        self.parent = None
+        self.node = value
+        self.children = deque()
+
+def generate(edges, target):
+    temp = [None] * len(target)
+    
+    for s, e in edges:
+        if temp[s - 1] is None:
+            temp[s - 1] = Tree(s - 1)
+        if temp[e - 1] is None:
+            temp[e - 1] = Tree(e - 1)
+
+    for s, e in edges:
+        temp[s - 1].children.append(temp[e - 1])
+        temp[s - 1].children = deque(sorted(temp[s - 1].children, key=lambda x: x.node))
+        temp[e - 1].parent = temp[s - 1]
+    
     return temp
+
 def move(node):
-    if len(node.children)==0:
+    if not node.children:
         return node
+    
     while node.children:
-        next_node=node.children.popleft()
+        next_node = node.children.popleft()
         node.children.append(next_node)
-        node=next_node
+        node = next_node
+    
     return node
+
 def solution(edges, target):
-    root=generate(edges,target)[0]
-    ord,ord_cnt=[],{i:0 for i in range(len(target))}
-    flag=False
-    while not flag:
-        current_node=move(root).node
-        ord.append(current_node)
-        ord_cnt[current_node]+=1
-        flag=True
-        for i in ord_cnt:
-            if ord_cnt[i]<int(math.ceil(target[i]/3)):
-                flag=False
-                break
-            elif ord_cnt[i]>target[i]:
-                return [-1]
-    temp=[1]*len(ord)
-    for i in ord:
-        target[i]-=1
-    for i in range(len(ord)-1,-1,-1):
-        if target[ord[i]]>=2:
-            temp[i]+=2
-            target[ord[i]]-=2
-        elif target[ord[i]]==1:
-            temp[i]+=1
-            target[ord[i]]-=1
-        else:
-            continue
-    return temp
+    root = generate(edges, target)[0]
+    order = []
+    order_cnt = {i: 0 for i in range(len(target))}
+    
+    while True:
+        cur_node = move(root).node
+        order.append(cur_node)
+        order_cnt[cur_node] += 1
+
+        if any(order_cnt[i] > target[i] for i in range(len(target))):
+            return [-1]
+        
+        if all(order_cnt[i] >= math.ceil(target[i] / 3) for i in range(len(target))):
+            break
+
+    result = [1] * len(order)
+    
+    for i in order:
+        target[i] -= 1
+    
+    for i in range(len(order) - 1, -1, -1):
+        idx = order[i]
+        if target[idx] >= 2:
+            result[i] += 2
+            target[idx] -= 2
+        elif target[idx] == 1:
+            result[i] += 1
+            target[idx] -= 1
+    
+    return result
