7962b2cd0b227c7055991d5864e61ee4ea60a69c
sungyeong <149341161+sungyeong98@users.noreply.github.com>
2025-06-30 21:50:24 +0900

    [level 4] Title: 1,2,3 떨어트리기, Time: 6.01 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/4/150364. 1，2，3 떨어트리기/1，2，3 떨어트리기.py
+++ 프로그래머스/4/150364. 1，2，3 떨어트리기/1，2，3 떨어트리기.py
@@ -7,6 +7,7 @@ class Tree:
         self.node = value
         self.children = deque()
 
+# 노드 별 정보 생성
 def generate(edges, target):
     temp = [None] * len(target)
     
@@ -23,6 +24,7 @@ def generate(edges, target):
     
     return temp
 
+# 리프 노드까지 이동하는 메서드
 def move(node):
     if not node.children:
         return node
@@ -35,26 +37,36 @@ def move(node):
     return node
 
 def solution(edges, target):
-    root = generate(edges, target)[0]
-    order = []
-    order_cnt = {i: 0 for i in range(len(target))}
+    root = generate(edges, target)[0]                   # 루트 노드
+    order = []                                          # 순번을 저장하는 리스트
+    order_cnt = {i: 0 for i in range(len(target))}      # 각 노드 별로 몇 번 도달하는지 갯수 저장
     
     while True:
-        cur_node = move(root).node
-        order.append(cur_node)
-        order_cnt[cur_node] += 1
+        cur_node = move(root).node      # 리프 노드
+        order.append(cur_node)          # order에 리프 노드 번호 저장
+        order_cnt[cur_node] += 1        # order_cnt에 +1
 
+        # 값을 가질 수 없는 노드에 도달한 적이 있으면 [-1]을 리턴
+        # 혹은 방문 횟수가 target 값보다 커지면 [-1]을 리턴
         if any(order_cnt[i] > target[i] for i in range(len(target))):
             return [-1]
         
+        # 방문한 적이 있는 노드들에 대해서 방문 횟수가 target[i]/3의 반올림 이상이면 만족
+        # 각 노드에는 최대 3까지 배정 가능
+        # 타겟 값이 8이라고 가정, 최대값 3 -> ceil(8/3) = 3
+        # 즉, 3번 이상 방문해야 노드에 원하는 타겟 값을 넣을 수 있음
+        # 타겟값이 필요한 모든 노드의 방문횟수가 적절히 채워질 때까지 반복
         if all(order_cnt[i] >= math.ceil(target[i] / 3) for i in range(len(target))):
             break
 
-    result = [1] * len(order)
+    result = [1] * len(order)   # 초기 방문 값은 1로 통일(어차피 1은 무조건 들어가야 됨)
     
+    # 타겟에서 1씩 제거 (result에 1씩 넣어줬으니까)
     for i in order:
         target[i] -= 1
     
+    # 뒤에서부터 돌면서 값을 채워넣어줌
+    # 적은 숫자를 사용해야 되고, 사전순으로 빠른 값을 찾아야 되기 때문
     for i in range(len(order) - 1, -1, -1):
         idx = order[i]
         if target[idx] >= 2:
